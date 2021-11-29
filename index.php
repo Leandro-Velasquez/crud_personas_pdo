227651fe3634491f247d899cacc8ee1c55e635fc
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-11-29 01:20:52 -0300

    Terminamos de darle estilos a la section tabla-datos

--- index.php
+++ index.php
@@ -8,43 +8,46 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <section class="registro">
-        <form class="formulario" action="">
-            <h2 class="formulario__h2">registrar persona</h2>
-            <div class="formulario__div">
-                <label class="formulario__label" for="nombre">Nombre:</label>
-                <input class="formulario__input" id="nombre" type="text" name="nombre">
-                <label class="formulario__label" for="apellido">Apellido:</label>
-                <input class="formulario__input" id="apellido" type="text" name="apellido">
-                <label class="formulario__label" for="telefono">Telefono:</label>
-                <input class="formulario__input" id="telefono" type="text" name="telefono">
-                <label class="formulario__label" for="email">Email:</label>
-                <input class="formulario__input" id="email" type="text" name="email">
-                <input class="formulario__button" type="submit" value="registrar">
-            </div>
-        </form>
-    </section>
-    <section class="tabla-datos">
-        <table>
-            <thead>
-                <th>Nombre</th>
-                <th>Apellido</th>
-                <th>Telefono</th>
-                <th colspan="2">Email</th>
-            </thead>
-            <tbody>
-                <tr>
-                   <td>asdasd</td>
-                   <td>sssssssss</td>
-                   <td>00000000</td>
-                   <td>asdasdas@gmail.com</td>
-                   <td>
-                       <a href="#">Editar</a>
-                       <a href="#">Eliminar</a>
-                    </td> 
-                </tr>
-            </tbody>
-        </table>
-    </section>
+    <div class="div-container-flex">
+        <section class="registro">
+            <form class="formulario" action="">
+                <h2 class="formulario__h2">registrar persona</h2>
+                <div class="formulario__div">
+                    <label class="formulario__label" for="nombre">Nombre:</label>
+                    <input class="formulario__input" id="nombre" type="text" name="nombre">
+                    <label class="formulario__label" for="apellido">Apellido:</label>
+                    <input class="formulario__input" id="apellido" type="text" name="apellido">
+                    <label class="formulario__label" for="telefono">Telefono:</label>
+                    <input class="formulario__input" id="telefono" type="text" name="telefono">
+                    <label class="formulario__label" for="email">Email:</label>
+                    <input class="formulario__input" id="email" type="text" name="email">
+                    <input class="formulario__button" type="submit" value="registrar">
+                </div>
+            </form>
+        </section>
+        <section class="tabla-datos">
+            <table class="table">
+                <caption class="table__caption">personas registradas</caption>
+                <thead class="table__thead">
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Telefono</th>
+                    <th colspan="2">Email</th>
+                </thead>
+                <tbody class="table__tbody">
+                    <tr>
+                        <td>asdasd</td>
+                        <td>sssssssss</td>
+                        <td>00000000</td>
+                        <td>asdasdas@gmail.com</td>
+                        <td class="table__container-buttons">
+                            <a href="#" class="table__button table__button--modified">Editar</a>
+                            <a href="#" class="table__button">Eliminar</a>
+                        </td> 
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    </div>
 </body>
 </html>

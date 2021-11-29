dad518bdca7c1bc71202bee12eecd6876244317b
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-11-29 00:36:47 -0300

    Terminamos de darle estilo a la section registro

--- index.php
+++ index.php
@@ -5,37 +5,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro personas</title>
-    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <section id="registro">
-        <form action="">
-            <h2>registrar persona</h2>
-            <label for="nombre">Nombre:</label>
-            <input id="nombre" type="text" name="nombre">
-            <label for="apellido">Apellido:</label>
-            <input id="apellido" type="text" name="apellido">
-            <label for="telefono">Telefono:</label>
-            <input id="telefono" type="text" name="telefono">
-            <label for="email">Email:</label>
-            <input id="email" type="text" name="email">
-            <input type="submit" value="Registrar">
+    <section class="registro">
+        <form class="formulario" action="">
+            <h2 class="formulario__h2">registrar persona</h2>
+            <div class="formulario__div">
+                <label class="formulario__label" for="nombre">Nombre:</label>
+                <input class="formulario__input" id="nombre" type="text" name="nombre">
+                <label class="formulario__label" for="apellido">Apellido:</label>
+                <input class="formulario__input" id="apellido" type="text" name="apellido">
+                <label class="formulario__label" for="telefono">Telefono:</label>
+                <input class="formulario__input" id="telefono" type="text" name="telefono">
+                <label class="formulario__label" for="email">Email:</label>
+                <input class="formulario__input" id="email" type="text" name="email">
+                <input class="formulario__button" type="submit" value="registrar">
+            </div>
         </form>
     </section>
-    <section id="tabla-datos">
+    <section class="tabla-datos">
         <table>
             <thead>
                 <th>Nombre</th>
                 <th>Apellido</th>
                 <th>Telefono</th>
-                <th>Email</th>
+                <th colspan="2">Email</th>
             </thead>
             <tbody>
                 <tr>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
+                   <td>asdasd</td>
+                   <td>sssssssss</td>
+                   <td>00000000</td>
+                   <td>asdasdas@gmail.com</td>
                    <td>
                        <a href="#">Editar</a>
                        <a href="#">Eliminar</a>

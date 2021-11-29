a0171631dbbae520110a7f8df6f1c99c8eee8e40
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-11-28 23:54:33 -0300

    Definimos la estructura HTML de la section registro y de la tabla de datos

--- index.php
+++ index.php
@@ -8,6 +8,41 @@
     <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-    
+    <section id="registro">
+        <form action="">
+            <h2>registrar persona</h2>
+            <label for="nombre">Nombre:</label>
+            <input id="nombre" type="text" name="nombre">
+            <label for="apellido">Apellido:</label>
+            <input id="apellido" type="text" name="apellido">
+            <label for="telefono">Telefono:</label>
+            <input id="telefono" type="text" name="telefono">
+            <label for="email">Email:</label>
+            <input id="email" type="text" name="email">
+            <input type="submit" value="Registrar">
+        </form>
+    </section>
+    <section id="tabla-datos">
+        <table>
+            <thead>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Telefono</th>
+                <th>Email</th>
+            </thead>
+            <tbody>
+                <tr>
+                   <td></td>
+                   <td></td>
+                   <td></td>
+                   <td></td>
+                   <td>
+                       <a href="#">Editar</a>
+                       <a href="#">Eliminar</a>
+                    </td> 
+                </tr>
+            </tbody>
+        </table>
+    </section>
 </body>
 </html>

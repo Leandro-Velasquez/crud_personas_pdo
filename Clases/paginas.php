2617a0bf432d6803569896e966e1c44ad31f1b54
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-13 23:09:34 -0300

    Definimos el constructor y el metodo calcularNumeroDePaginas de la clase Paginas

--- Clases/paginas.php
+++ Clases/paginas.php
@@ -1,5 +1,18 @@
 <?php
-    class Paginas{
+    class Paginas extends Persona{
+        private $numeroDePaginas;
+        private $totalRegistros;
+        private $registrosPorPagina;
         
+        public function __construct($registrosPorPagina)
+        {
+            parent::__construct("localhost:3307", "personas_crud_pdo", "root", "");
+            $this->registrosPorPagina = $registrosPorPagina;
+            $this->totalRegistros = $this->getCantidadRegistros();
+        }
+
+        public function calcularNumeroDePaginas(){
+            $this->numeroDePaginas = ceil($this->totalRegistros / $this->registrosPorPagina);
+        }
     }
 ?>

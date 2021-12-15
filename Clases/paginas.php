55cc6fcf923f7a5fe73b4ad83e27e83cf77a291f
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-14 23:21:14 -0300

    Definimos el metodo botones de la clase Paginas

--- Clases/paginas.php
+++ Clases/paginas.php
@@ -9,10 +9,23 @@
             parent::__construct("localhost:3307", "personas_crud_pdo", "root", "");
             $this->registrosPorPagina = $registrosPorPagina;
             $this->totalRegistros = $this->getCantidadRegistros();
+            $this->calcularNumeroDePaginas();
         }
 
         public function calcularNumeroDePaginas(){
             $this->numeroDePaginas = ceil($this->totalRegistros / $this->registrosPorPagina);
         }
+
+        public function botones(){
+            echo '<ul class="paginas">';
+            for($i = 0; $i < $this->numeroDePaginas; $i++){
+                ?>
+                <li><a class="paginas__boton" href="index.php?pagina=<?php echo ($i + 1); ?>"><?php echo ($i + 1); ?></a></li>
+                <?php
+            }
+            echo '</ul>';
+        }
+
+        
     }
 ?>

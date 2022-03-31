844f99dab7984d0a0c7b6e7f9f757d25eec0ca73
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-03-31 20:42:46 -0300

    Agregamos el parametro params_array a los metodos view y content

--- Clases/Controller.php
+++ Clases/Controller.php
@@ -1,18 +1,20 @@
 <?php
 class Controller
 {
+
     public function index()
     {
         return $this->view('crud/index');
     }
 
-    public function view($routeView)
+    public function view($routeView, $params_array = null)
     {
         require "./View/index.php";
     }
 
-    public function content($view)
+    public function content($view, $params_array = null)
     {
+        extract($params_array);
         require "./View/" . $view . ".php";
     }
 }

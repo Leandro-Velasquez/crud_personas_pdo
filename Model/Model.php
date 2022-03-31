e7128fac8d8505affcaad5320f5b8e672ac1187f
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-03-31 18:53:25 -0300

    Definimos el metodo all() de la clase Model

--- Model/Model.php
+++ Model/Model.php
@@ -1,16 +1,26 @@
 <?php
 class Model {
     protected $pdo;
+    private $table;
 
-    public function __construct()
+    public function __construct($table)
     {
         try{
             $this->pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";" , DB_USER, DB_PASSWORD);
+            $this->table = $table;
         }catch(PDOException $e){
             die("Error al conectar con la base de datos: " . $e->getMessage());
         }catch(Exception $e){
             die("Error generico: " . $e->getMessage());
         }
+    }
+
+    public function all()
+    {
+        $sql = 'SELECT * FROM ' . $this->table;
+        $objectPdo = $this->pdo->query($sql);
+        $resultados = $objectPdo->fetchAll(PDO::FETCH_CLASS);
+        return $resultados;
     }
 
     public function connect()

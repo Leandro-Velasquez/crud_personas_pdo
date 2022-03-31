6d279dd38fd1c9b927b5039e830baf6bccb35230
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-03-31 14:48:56 -0300

    Agregamos el archivo config.php y definimos algunas constantes

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'personas_crud_pdo');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('URL_SITE', 'http://localhost/crud_personas_pdo/');

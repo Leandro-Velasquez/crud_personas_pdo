3dc8ff5fc37a5aa9df109cb3fced6585cbde6e43
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-04-01 18:04:25 -0300

    Declaramos las propiedades nombre, apellido, telefono, email y definimos el metodo save()

--- Model/Persona.php
+++ Model/Persona.php
@@ -4,9 +4,23 @@ require "Model.php";
 class Persona extends Model
 {
     private $table = "personas";
+    public $nombre;
+    public $apellido;
+    public $telefono;
+    public $email;
 
     public function __construct()
     {
         parent::__construct($this->table);
+    }
+
+    public function save()
+    {
+        $gsent = $this->connect()->prepare('INSERT INTO personas (nombre, apellido, telefono, email) VALUES (:nombre, :apellido, :telefono, :email)');
+        $gsent->bindParam(':nombre', $this->nombre, PDO::PARAM_STR, 60);
+        $gsent->bindParam(':apellido', $this->apellido, PDO::PARAM_STR, 60);
+        $gsent->bindParam(':telefono', $this->telefono, PDO::PARAM_STR, 45);
+        $gsent->bindParam(':email', $this->email, PDO::PARAM_STR, 255);
+        $gsent->execute();
     }
 }

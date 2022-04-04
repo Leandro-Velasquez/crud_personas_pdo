f3942f6ac3581287e896d421be733ba80e9c0fe5
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-04-03 23:52:02 -0300

    Eliminamos el metodo index de la clase Controller

--- Clases/Controller.php
+++ Clases/Controller.php
@@ -2,11 +2,6 @@
 class Controller
 {
 
-    public function index()
-    {
-        return $this->view('crud/index');
-    }
-
     public function view($routeView, $params_array = null)
     {
         require "./View/index.php";

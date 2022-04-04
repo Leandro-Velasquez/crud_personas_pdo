448d1ed5a8bf55c4953eab02fb8a82857929ca10
Leandro Velasquez <leandro.velasquez032@gmail.com>
2022-04-04 00:18:19 -0300

    Definimos los metodos paginate() y links() de la clase Model

--- Model/Model.php
+++ Model/Model.php
@@ -1,7 +1,11 @@
 <?php
+require "./Clases/PaginasMenu.php";
+
 class Model {
     protected $pdo;
     private $table;
+    public $limit = 5;
+    public $offset = 0;
 
     public function __construct($table)
     {
@@ -19,7 +23,8 @@ class Model {
     {
         $sql = 'SELECT * FROM ' . $this->table;
         $objectPdo = $this->pdo->query($sql);
-        $resultados = $objectPdo->fetchAll(PDO::FETCH_CLASS);
+        $objectPdo->setFetchMode(PDO::FETCH_CLASS, get_class($this));
+        $resultados = $objectPdo->fetchAll();
         return $resultados;
     }
 
@@ -30,6 +35,37 @@ class Model {
         $objectPdo->execute();
         $objectPdo->setFetchMode(PDO::FETCH_CLASS, get_class($this));
         return $objectPdo->fetch();
+    }
+
+    public function paginate()
+    {
+        $url = "";
+        if(substr($_GET['url'], -1) == '/') $url = substr($_GET['url'], 0, -1);
+        else $url = $_GET['url'];
+        if(count(explode('/', $url)) > 2)
+        {
+            $page = substr(explode('/', $url)[2], -1);
+        }
+        else $page=1;
+        $this->offset = ($page - 1) * $this->limit;
+        $objectPdo = $this->pdo->prepare('SELECT * FROM ' . $this->table . ' LIMIT :limit OFFSET :offset');
+        $objectPdo->bindParam(':limit', $this->limit, PDO::PARAM_INT);
+        $objectPdo->bindParam(':offset', $this->offset, PDO::PARAM_INT);
+        $objectPdo->execute();
+        $objectPdo->setFetchMode(PDO::FETCH_CLASS, get_class($this));
+        return (new PaginasMenu($objectPdo->fetchAll(), $this->getCantidadRegistros(), $this->limit));
+    }
+
+    public function links()
+    {
+        return require "./View/Components/paginacion.php";
+    }
+
+    public function getCantidadRegistros()
+    {
+        $resultado = $this->pdo->query('SELECT COUNT(*) AS cantidad_registros FROM ' . $this->table);
+
+        return $resultado->fetch(PDO::FETCH_ASSOC)['cantidad_registros'];
     }
 
     public function connect()
